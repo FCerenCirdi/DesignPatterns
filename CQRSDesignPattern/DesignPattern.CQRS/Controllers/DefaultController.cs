e908f889c589f020bdf0d18307339e2853cc3242
Ceren CIRDI <fcerencirdi@hotmail.com>
2023-08-10 17:33:57 +0300

    CQRSDesignPattern

---
+++ CQRSDesignPattern/DesignPattern.CQRS/Controllers/DefaultController.cs
@@ -0,0 +1,46 @@
+﻿using AspNetCore;
+using DesignPattern.CQRS.CQRSPattern.Commands;
+using DesignPattern.CQRS.CQRSPattern.Handlers;
+using DesignPattern.CQRS.CQRSPattern.Queries;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DesignPattern.CQRS.Controllers
+{
+    public class DefaultController : Controller
+    {
+        private readonly GetProductQueryHandler _getProductQueryHandler;
+        private readonly CreateProductCommandHandler _createProductCommandHandler;
+        private readonly GetProductByIDQueryHandler _getProductByIDQueryHandler;
+
+        public DefaultController(GetProductQueryHandler getProductQueryHandler, CreateProductCommandHandler createProductCommandHandler, GetProductByIDQueryHandler getProductByIDQueryHandler)
+        {
+            _getProductQueryHandler = getProductQueryHandler;
+            _createProductCommandHandler = createProductCommandHandler;
+            _getProductByIDQueryHandler = getProductByIDQueryHandler;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _getProductQueryHandler.Handle();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult AddProduct()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddProduct(CreateProductCommand command )
+        {
+            _createProductCommandHandler.Handler(command);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult GetProduct(int id) 
+        {
+            var values = _getProductByIDQueryHandler.Handle(new GetProductByIDQuery(id));
+            return View(values);
+        }
+    }
+}
